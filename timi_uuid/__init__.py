c379d3fda8bd5ab1a15981c76fb884f52ffa5234
lixiaolong <lixiaolong@sensoro.com>
2018-11-01 17:20:18 +0800

    version: 0.0.6

--- timi_uuid/__init__.py
+++ timi_uuid/__init__.py
@@ -1,5 +1,34 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
+
+#   __
+#  /__)  _  _     _   _ _/   _
+# / (   (- (/ (/ (- _)  /  _)
+#          /
+
+"""
+Description: single machine generate auto-incrementing uuid str.
+
+Examples:
+    >>> from timie_uuid import TimiUUID
+
+    >>> obj = TimiUUID()
+
+    >>> obj.get_id()
+    '8c288f25-d690-4600-f911-e6011175d101'
+
+    >>> timi_uuid.get_id_info(cnt_id="8c288f25-d690-4600-f911-e6011175d101")
+    # {
+    #     'timestamp': 1541058282.02128,
+    #     'seq': 1,
+    #     'pid': 5982,
+    #     'mac': '46:00:f9:11:e6:01',
+    #     'msg': 'Success.',
+    #     'datetime': '2018-11-01 15:44:42.021280'
+    # }
+
+"""
+
 
 from .main_uuid import TimiUUID
 
-__version__ = "0.0.5"
+from .__version__ import __version__, __title__, __description__, __url__, __author_email__
---
+++ timi_uuid/__version__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "0.0.6"
+__title__ = "timi-uuid"
+__description__ = "A sensible class for dealing with single machine generate auto-incrementing uuid str."
+__url__ = "https://github.com/lxl0928/timi_uuid"
+__author_email__ = "lixiaolong@smuer.cn"
